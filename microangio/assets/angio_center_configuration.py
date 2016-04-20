e27764ecab1a6ab5d35a054a3e8103ccf67305b1
nharringtonwasatch <nharrington@wasatchphotonics.com>
2016-04-20 16:59:42 -0400

    rebuilt for windows

--- microangio/assets/angio_center_configuration.py
+++ microangio/assets/angio_center_configuration.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'microangio/assets/angio_center_configuration.ui'
 #
-# Created: Wed Apr 20 14:10:57 2016
+# Created: Wed Apr 20 14:58:31 2016
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
